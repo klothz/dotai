7093d90b51c22d5f44d0ca261ba7f8cf1570b6fa
GateauXD <dantran2016@gmail.com>
2020-02-07 13:08:10 -0800

    Added some cookie functions

--- js/utils.js
+++ js/utils.js
@@ -15,3 +15,25 @@ $(function () {
         zIndexStart: 8, // because of absolute position, set z-index base value
     });
 });
+
+function createCookie(key, value) {
+    let cookie = escape(key) + "=" + escape(value) + ";"
+    document.cookie = cookie;
+    console.log(cookie);
+    console.log("New cookie with key: " + key + " value: " + value);
+ }
+
+ function readCookie(name) {
+    let key = name + "=";
+    let cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(key) === 0) {
+            return cookie.substring(key.length, cookie.length);
+        }
+    }
+    return null;
+}
