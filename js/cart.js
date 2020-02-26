3e14aa8403a184175a539569b135fc70db54024f
GateauXD <dantran2016@gmail.com>
2020-02-25 20:41:55 -0800

    Added comments to my new idea for carts

---
+++ js/cart.js
@@ -0,0 +1,25 @@
+// Will be used to add items to the shopping cart currently I need to think about how to uniquely
+// identify items with sizes. 
+
+// Currently thinking about doing: {itemID} + 'breakchar' + size can be M or 30 or OS 
+function addItem(itemID){
+    return;
+}
+
+// This function will be used when in the cart and the user wants to add items for example 3 of the same shirts
+function modifyCount(itemID){
+    return;
+}
+
+function deleteItem(itemID){
+    return;
+}
+
+function clearCart(itemID){
+    return;
+}
+
+// Iterates through the session values and sums up the entire price.
+function sumCartValues(itemID){
+    return;
+}
