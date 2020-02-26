7753c45b1ca223f15a8789c8e87e25ed38a2908e
GateauXD <dantran2016@gmail.com>
2020-02-26 12:10:56 -0800

    Basic logic for addItem is written

--- js/cart.js
+++ js/cart.js
@@ -1,25 +1,34 @@
 // Will be used to add items to the shopping cart currently I need to think about how to uniquely
 // identify items with sizes. 
+var breakCharacter = "\xA9"
+function addItem(itemString) {
+    // Check if the item is already in the database if so just ++ the value.
+    if (sessionStorage.getItem(itemString) != null) {
+        let itemCount = sessionStorage.getItem(itemString);
+        sessionStorage.setItem(itemString, itemCount + 1);
+    }
+    else {
+        // Save the item to the database with value 1.
+        sessionStorage.setItem(itemString, 1);
+    }
 
-// Currently thinking about doing: {itemID} + 'breakchar' + size can be M or 30 or OS 
-function addItem(itemID){
-    return;
+    console.log(sessionStorage)
 }
 
 // This function will be used when in the cart and the user wants to add items for example 3 of the same shirts
-function modifyCount(itemID){
+function modifyCount(itemString, amount) {
     return;
 }
 
-function deleteItem(itemID){
+function deleteItem(itemString) {
     return;
 }
 
-function clearCart(itemID){
+function clearCart() {
     return;
 }
 
 // Iterates through the session values and sums up the entire price.
-function sumCartValues(itemID){
+function sumCartValues() {
     return;
 }
